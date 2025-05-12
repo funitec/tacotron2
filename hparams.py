126d9c0f34aed2bed694fce9963706b14f1b3982
funitec <koufuku293@gmial.com>
2025-05-12 14:03:15 +0900

    ver1.1

--- hparams.py
+++ hparams.py
@@ -18,7 +18,7 @@ def create_hparams(hparams_string=None):
         ################################
         # Experiment Parameters        #
         ################################
-        epochs=500,
+        epochs=100,
         iters_per_checkpoint=1000,
         seed=1234,
         dynamic_loss_scaling=True,
@@ -90,7 +90,7 @@ def create_hparams(hparams_string=None):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=64,
+        batch_size=32,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
